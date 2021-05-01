1abd26d1905c18153e1d8dac870056f56b58ef83
wocat <wocat@Alien>
2021-05-01 15:08:19 +0200

    Element removal and basic element editor

--- src/dansesben/frontend/App.svelte
+++ src/dansesben/frontend/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import {Col, Container, Row, Table, FormGroup, Label, Input
-        , InputGroup, InputGroupText, Button} from 'sveltestrap';
+        , InputGroup, InputGroupText, Button, Icon, Modal, ModalBody,
+        ModalFooter, ModalHeader, Form, Alert} from 'sveltestrap';
     import { onMount } from 'svelte';
 
     let data = [];
@@ -13,6 +14,13 @@
         data = await res.json();
     })
 
+    //Load data
+
+    async function loadData(){
+        const res = await fetch('/api/v1/performances-by-degrees-us/');
+        data = await res.json();
+    }
+
     function previousPage() {
         if (currentPage > 1) currentPage--;
     }
@@ -21,7 +29,112 @@
         if (currentPage < data.length / maxElements) currentPage++;
     }
 
+    // View modal
+    let openViewModal = false;
+    let editModalIndex = 0;
+    const toggleViewModal = () => (openViewModal = !openViewModal);
+
+    function editElement(index){
+        editModalIndex = index;
+        toggleViewModal();
+    }
+
+    // Delete modal
+    let deleteModal = false;
+    let deleteModalIndex = 0;
+    const toggleDeleteModal = () => (deleteModal = !deleteModal);
+
+    function openDeleteModal(index){
+        deleteModalIndex = index;
+        toggleDeleteModal();
+    }
+
+    // Alert
+    let alerts = [];
+
+    async function deleteElement(){
+        let element = data[deleteModalIndex];
+        let res = await fetch('/api/v1/performances-by-degrees-us/' + element.center + "/" + element.year + "/" + element["field-of-knowledge"], {"method": "DELETE"});
+
+        if(res.status === 200){
+            alerts = [...alerts, {text: "Eliminado " + element.center + "/" + element.year + "/" + element["field-of-knowledge"] + " correctamente", color: "success"}];
+            setTimeout(() => {alerts.shift(); alerts = alerts;}, 3000);
+        }else if(res.status === 404){
+            alerts = [...alerts, {text: "Elemento " + element.center + "/" + element.year + "/" + element["field-of-knowledge"] + " no existe", color: "warning"}];
+            setTimeout(() => {alerts.shift(); alerts = alerts;}, 3000);
+        }else{
+            alerts = [...alerts, {text: "Error interno al intentar borrar " + element.center + "/" + element.year + "/" + element["field-of-knowledge"], color: "danger"}];
+            setTimeout(() => {alerts.shift(); alerts = alerts;}, 3000);
+        }
+
+        toggleDeleteModal();
+        await loadData(); // Renew data
+    }
+
+
 </script>
+
+<!-- Alert -->
+<Container>
+    {#each alerts as alert}
+        <Alert color={alert.color} class="mb-1" dismissible>
+            {alert.text}
+        </Alert>
+    {/each}
+</Container>
+
+<!-- Delete Modal -->
+<div>
+    <Modal isOpen={deleteModal} {toggleDeleteModal}>
+        <ModalHeader {toggleDeleteModal}>¿Seguro que quiere borrar el elemento?</ModalHeader>
+        <ModalBody>
+            El elemento será permanentemente borrado
+        </ModalBody>
+        <ModalFooter>
+            <Button color="danger" on:click={deleteElement}>Borrar</Button>
+            <Button color="secundary" on:click={toggleDeleteModal}>Cerrar</Button>
+        </ModalFooter>
+    </Modal>
+</div>
+<!-- Edit modal -->
+<div>
+    <Modal isOpen={openViewModal} {toggleViewModal}>
+        <ModalHeader {toggleViewModal}>Editar entrada</ModalHeader>
+        <ModalBody>
+
+            <Form>
+                <FormGroup>
+                    <Label><b>Campo de conocimiento</b></Label>
+                    <Input type="text" value={data[editModalIndex]["field-of-knowledge"]} />
+                </FormGroup>
+                <FormGroup>
+                    <Label><b>Año</b></Label>
+                    <Input type="number" value={data[editModalIndex].year} />
+                </FormGroup>
+                <FormGroup>
+                    <Label><b>Rendimiento</b></Label>
+                    <Input type="text" value={data[editModalIndex]["performance-percents"]} />
+                </FormGroup>
+                <FormGroup>
+                    <Label><b>Créditos superados</b></Label>
+                    <Input type="number" value={data[editModalIndex]["credits-passed"]} />
+                </FormGroup>
+                <FormGroup>
+                    <Label><b>Créditos matriculados</b></Label>
+                    <Input type="number" value={data[editModalIndex]["credits-enrolled"]} />
+                </FormGroup>
+                <FormGroup>
+                    <Label><b>Centro</b></Label>
+                    <Input type="text" value={data[editModalIndex].center} />
+                </FormGroup>
+            </Form>
+        </ModalBody>
+        <ModalFooter>
+            <Button color="primary" on:click={toggleViewModal}>Guardar</Button>
+            <Button color="secundary" on:click={toggleViewModal}>Cerrar</Button>
+        </ModalFooter>
+    </Modal>
+</div>
 
 <!-- Title -->
 <Container>
@@ -34,6 +147,8 @@
 <Container>
     <Row>
         <Col>
+        </Col>
+        <Col>
             <FormGroup>
                 <Label>Elementos</Label>
                 <Input type="select" bind:value={maxElements} on:change={() => {currentPage = 1}}>
@@ -44,12 +159,10 @@
                 </Input>
             </FormGroup>
         </Col>
-        <Col>
-        </Col>
     </Row>
     <Row>
         <Col>
-            <Table class="mt-4" responsive>
+            <Table responsive>
                 <thead>
                 <tr>
                     <th>Campo de conocimiento</th>
@@ -58,10 +171,12 @@
                     <th>Créditos superados</th>
                     <th>Créditos matriculados</th>
                     <th>Centro</th>
+                    <th>Acciones</th>
                 </tr>
                 </thead>
                 <tbody>
-                {#each data.slice(maxElements * (currentPage - 1), maxElements * currentPage) as row}
+                {#each data.slice(maxElements * (currentPage - 1), maxElements * currentPage) as row,
+                    index ({"year": row.year, "center": row.center, "field-of-knowledge": row["field-of-knowledge"]})}
                     <tr>
                         <td>{row["field-of-knowledge"]}</td>
                         <td>{row.year}</td>
@@ -69,6 +184,12 @@
                         <td>{row["credits-passed"]}</td>
                         <td>{row["credits-enrolled"]}</td>
                         <td>{row.center}</td>
+                        <td>
+                            <FormGroup>
+                                <Button color="primary" on:click={() => editElement(index)}><Icon name="pencil-fill"/></Button>
+                                <Button color="danger" on:click={() => openDeleteModal(index)}><Icon name="trash-fill"/></Button>
+                            </FormGroup>
+                        </td>
                     </tr>
                 {/each}
                 </tbody>
