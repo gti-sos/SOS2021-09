857608c3acd497a2544fdc9d00c8e9adc61f5d5a
Carlos González Arenas <cargonare1@alum.us.es>
2021-05-04 03:41:39 +0200

    cam

--- src/cargonare1/frontend/routes/LandingPage.svelte
+++ src/cargonare1/frontend/routes/LandingPage.svelte
@@ -1,5 +1,5 @@
 
 <main>
 	<h2>Página de Inicio de la interfaz gráfica de usuario</h2>
-	<a href="#/surrenders">Click aquí para ver la tabla de abandonos</a>
+	<a href="#/surrenders/">Click aquí para ver la tabla de abandonos</a>
 </main>
