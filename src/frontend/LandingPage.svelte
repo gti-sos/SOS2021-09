5b981d8dfefc3de5c5879b9ce3d5750883c36317
wocat <wocat@Alien>
2021-05-15 20:24:37 +0200

    Adding container to the landingpage

--- src/frontend/LandingPage.svelte
+++ src/frontend/LandingPage.svelte
@@ -1,7 +1,12 @@
+<script>
+	import {Container} from 'sveltestrap';
+</script>
 
-<main>
-	<h2>Página de Inicio del grupo 09</h2>
-	<a href="#/info">Click aquí para ver la info de nuestro grupo</a>
-	<p></p>
-	<a href="#/graphs">Click aquí para ver las graficas de nuestro grupo</a>
-</main>
+<Container>
+	<main class="text-center pt-3">
+		<h2>Página de Inicio del grupo 09</h2>
+		<a href="#/info">Click aquí para ver la info de nuestro grupo</a>
+		<p></p>
+		<a href="#/graphs">Click aquí para ver las graficas de nuestro grupo</a>
+	</main>
+</Container>
