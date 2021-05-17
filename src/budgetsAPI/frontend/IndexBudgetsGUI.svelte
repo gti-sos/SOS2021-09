0d9e23a1777444ca25e81bea405e6df1a03bd9ad
Adrirofer <adrianromanfernandez@gmail.com>
2021-05-17 15:00:02 +0200

    gráficos adri y push del carlo

--- src/budgetsAPI/frontend/IndexBudgetsGUI.svelte
+++ src/budgetsAPI/frontend/IndexBudgetsGUI.svelte
@@ -5,13 +5,15 @@
 	import budgetsIndex from './routes/budgetsTable.svelte';
 	import BudgetEdit from './routes/editBudget.svelte';
 	import BudgetsSearch from './routes/searchBudget.svelte';
+	import BudgetsGraph from './routes/budgetsGraph.svelte';
 //	import NotFound from './NotFound.svelte';
 
 	const routes = {
 		"/": Landing,
 		"/budgets": budgetsIndex,
 		"/budgets/:center/:year": BudgetEdit,
-		"/budgets/search": BudgetsSearch
+		"/budgets/search": BudgetsSearch,
+		"/budgets/graphs": BudgetsGraph
 //		"*": NotFound
 	};
 
