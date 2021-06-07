b023da254113159e8dd17f995ccb25972297d24e
Fran <frankzzcsgo@gmail.com>
2021-06-07 18:58:33 +0200

    test e2e

--- src/fraferbla1/frontend/analytics/graphs.svelte
+++ src/fraferbla1/frontend/analytics/graphs.svelte
@@ -2,9 +2,9 @@
 	<h2>Página de gráficas de la API de fraferbla1</h2>
     <br>
 
-    <a href="/fraferbla1/#/cuts/graficas">Gráfica con la librería Highcharts</a>
+    <a id= "link-to-cutsgraph" href="/fraferbla1/#/cuts/graficas">Gráfica con la librería Highcharts</a>
     <br>
-    <a id= "link-to-cutsgraph" href="/fraferbla1/#/cuts/graficav2">Gráfica con la librería Quickchart</a>
+    <a href="/fraferbla1/#/cuts/graficav2">Gráfica con la librería Quickchart</a>
     <br>
 
 </main>
