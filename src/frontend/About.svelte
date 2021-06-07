b40f37944fbe0abe7fb05f346433550d9452f52e
Wocanilo <25706428+Wocanilo@users.noreply.github.com>
2021-06-07 21:46:54 +0200

    Uploaded videos

--- src/frontend/About.svelte
+++ src/frontend/About.svelte
@@ -10,7 +10,11 @@
     </Row>
     <Row>
         <Col>
-            <h4>Buedgets_by_Centers</h4>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/kyXHBLmyU7c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </Col>
+            <h4>Budgets_by_Centers</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/f90nbcuQXX0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </Col>
+        <Col>
+            <h4>Cut-off-marks-by-degrees-us</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/SdWv26g5TcY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </Col>
     </Row>
 </Container>
