8c04153dd2ca881828736a373995f6544c0b8f38
Adrirofer <adrianromanfernandez@gmail.com>
2021-06-07 16:20:51 +0200

    Reinicio heroku pa hacer vídeo

--- src/frontend/About.svelte
+++ src/frontend/About.svelte
@@ -8,4 +8,9 @@
             <h4>Performances-by-degrees</h4>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/kyXHBLmyU7c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </Col>
     </Row>
+    <Row>
+        <Col>
+            <h4>Buedgets_by_Centers</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/kyXHBLmyU7c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        </Col>
+    </Row>
 </Container>
